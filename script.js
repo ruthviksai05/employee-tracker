88ab3c81466b138992ae853e6aee3f36e1f6899e
ruthviksai05 <159747915+ruthviksai05@users.noreply.github.com>
2024-10-09 20:39:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -48,3 +48,27 @@ button:hover {
     color: red;
     margin-top: 10px;
 }
+
+document.getElementById('loginForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const response = await fetch('https://ptxgsnef2a.execute-api.us-west-2.amazonaws.com/prod/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    const data = await response.json();
+    
+    if (response.status === 200) {
+        document.getElementById('message').textContent = data.message;
+        document.getElementById('message').style.color = "green";
+    } else {
+        document.getElementById('message').textContent = data.message;
+        document.getElementById('message').style.color = "red";
+    }
+});
+
