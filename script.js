02d020f1ea766ec80aab44c862f79cb3bc593120
ruthviksai05 <159747915+ruthviksai05@users.noreply.github.com>
2024-10-09 21:53:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    const response = await fetch('https://ptxgsnef2a.execute-api.us-west-2.amazonaws.com/prod/login', {
+    const response = await fetch('http://localhost:3000/login', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' }
     });
 
     const data = await response.json();
-    
+
     if (response.status === 200) {
         document.getElementById('message').textContent = data.message;
         document.getElementById('message').style.color = "green";
